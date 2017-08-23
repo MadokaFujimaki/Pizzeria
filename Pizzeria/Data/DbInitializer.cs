82cc1d85f8e3caae4d76e718925736f134cc5781
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-08-23 13:39:22 +0200

    Changed to InMemoryDatabase

---
+++ Pizzeria/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Pizzeria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initializer(UserManager<ApplicationUser> userManager)
+        {
+
+        }
+    }
+}
