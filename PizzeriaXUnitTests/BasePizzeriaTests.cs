bc9a4fba152140065ab2fee7e16db7d7a6c85dfd
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-09-06 15:44:59 +0200

    Added PizzeriaXUnitTests

---
+++ PizzeriaXUnitTests/BasePizzeriaTests.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pizzeria.Controllers;
+using Pizzeria.Data;
+using Pizzeria.Services;
+using System;
+using Xunit;
+
+namespace PizzeriaXUnitTests
+{
+    public class BasePizzeriaTests
+    {
+        public readonly IServiceProvider serviceProvider;
+
+        public BasePizzeriaTests()
+        {
+            var efServiceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(b =>
+                b.UseInMemoryDatabase("Pizzadatabase")
+                .UseInternalServiceProvider(efServiceProvider));
+            services.AddTransient<CartService>();
+            //services.AddTransient<CategoryService>();
+            services.AddTransient<DishService>();
+            services.AddTransient<IngredientService>();
+            services.AddTransient<HomeController>();
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddTransient<ISession, TestSession>();
+
+            serviceProvider = services.BuildServiceProvider();
+            services.AddSingleton<IServiceProvider>(serviceProvider);
+
+            InitializeDatabase();
+        }
+
+        public virtual void InitializeDatabase()
+        {
+        }
+    }
+}
---
+++ PizzeriaXUnitTests/IngredientServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pizzeria.Data;
+using Pizzeria.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PizzeriaXUnitTests
+{
+    //public class IngredientServiceTests
+    //{
+    //    private readonly IServiceProvider _serviceProvider;
+
+    //    public IngredientServiceTests()
+    //    {
+    //        var efServiceProvider = new ServiceCollection()
+    //            .AddEntityFrameworkInMemoryDatabase()
+    //            .BuildServiceProvider();
+
+    //        var services = new ServiceCollection();
+
+    //        services.AddDbContext<ApplicationDbContext>(b =>
+    //        b.UseInMemoryDatabase("Pizzadatabas")
+    //        .UseInternalServiceProvider(efServiceProvider));
+
+    //        services.AddTransient<IngredientService>();
+
+    //        _serviceProvider = services.BuildServiceProvider();
+    //    }
+
+
+    //    [Fact]
+    //    public void All_Are_Sorted()
+    //    {
+    //        var _ingredients = _serviceProvider.GetService<IngredientService>();
+    //        var ings = _ingredients.AllIngredients();
+    //        Assert.Equal(ings.Count,0);
+    //    }
+    //}
+
+    public class IngredientServiceTests : BasePizzeriaTests
+    {
+        public override void InitializeDatabase()
+        {
+            base.InitializeDatabase();
+            var context = serviceProvider.GetService<ApplicationDbContext>();
+            context.Ingredients.Add(new Pizzeria.Models.Ingredient { Name = "BBB", Price = 2 });
+            context.Ingredients.Add(new Pizzeria.Models.Ingredient { Name = "AAA", Price = 5 });
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void All_Are_Sorted()
+        {
+            var _ingredients = serviceProvider.GetService<IngredientService>();//Arrange
+            var ings = _ingredients.AllIngredients();//Act
+            //Assert.Equal(ings.Count, 0);
+            Assert.Equal(2, ings.Count);//Assert
+            Assert.Equal(ings[0].Name, "AAA");
+            Assert.Equal(ings[1].Name, "BBB");
+        }
+    }
+}
+
